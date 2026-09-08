d770b47ba272da2b8c560500910208c6b6ab4c86
Behzad Shakouri <behzad.shakouri92@gmail.com>
2026-09-08 16:04:08 -0400

    Track GUI runtime mode and lock sensor hardware

--- main.cpp
+++ main.cpp
@@ -3,11 +3,21 @@
 /////////////////////////////////////////////////////////////
 
 #include "anacostiaiq.h"
+#include "RuntimeModeGuard.h"
+
 #include <QApplication>
+#include <QMessageBox>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+
+    RuntimeModeGuard modeGuard("gui");
+    if (!modeGuard.acquired()) {
+        QMessageBox::critical(nullptr, "AnacostiaIQ",
+                              modeGuard.errorString());
+        return 2;
+    }
 
     AnacostiaIQ window;
     window.show();
